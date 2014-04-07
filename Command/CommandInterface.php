b1c5e98816a4821cf4dafcb4768c3b82050cbeaf
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2014-04-08 00:38:53 +0200

    Add Command/Bus/Handler Interface

---
+++ Command/CommandInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Black package.
+ *
+ * (c) Alexandre Balmes <albalmes@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Black\Bundle\CommonBundle\Command;
+
+/**
+ * Class CommandInterface
+ *
+ * @package Black\Bundle\CommonBundle\Command
+ * @author  Alexandre Balmes <albalmes@gmail.com>
+ * @license http://opensource.org/licenses/mit-license.php MIT
+ */
+interface CommandInterface
+{
+
+} 
---
+++ Command/HandlerInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Black package.
+ *
+ * (c) Alexandre Balmes <albalmes@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Black\Bundle\CommonBundle\Command;
+
+/**
+ * Class HandlerInterface
+ *
+ * @author  Alexandre Balmes <albalmes@gmail.com>
+ * @license http://opensource.org/licenses/mit-license.php MIT
+ */
+interface HandlerInterface
+{
+    /**
+     *
+     */
+    public function execute();
+} 
