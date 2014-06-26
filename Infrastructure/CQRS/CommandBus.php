9c58647812d82fb6a288cf54918b4e604fcf3861
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2014-06-26 17:41:19 -0400

    Add CommandBus

---
+++ Infrastructure/CQRS/CommandBus.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * This file is part of the Black package.
+ *
+ * (c) Alexandre Balmes <alexandre@lablackroom.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Black\Bundle\CommonBundle\Infrastructure\CQRS;
+
+use Black\DDD\DDDinPHP\Infrastructure\CQRS\CommandBusInterface;
+use Black\DDD\DDDinPHP\Infrastructure\CQRS\CommandHandlerInterface;
+use Black\DDD\DDDinPHP\Infrastructure\CQRS\CommandInterface;
+
+/**
+ * Class Bus
+ *
+ * @author  Alexandre 'pocky' Balmes <alexandre@lablackroom.com>
+ * @license http://opensource.org/licenses/mit-license.php MIT
+ */
+final class CommandBus implements CommandBusInterface
+{
+    /**
+     * @var array
+     */
+    protected $handlers = array();
+
+    /**
+     * @param CommandInterface $command
+     * @param CommandHandlerInterface $handler
+     */
+    public function register($commandClassName, CommandHandlerInterface $handler)
+    {
+        $this->handlers[$commandClassName] = $handler;
+    }
+
+    /**
+     * @param $command
+     */
+    public function handle($command)
+    {
+        $this->handlers[get_class($command)]->handle($command);
+    }
+
+    public function getHandlers()
+    {
+        return $this->handlers;
+    }
+} 
