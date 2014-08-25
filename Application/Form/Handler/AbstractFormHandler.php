e3b5b354e60751a1b484141009841c0b16be2290
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2014-08-25 12:14:30 +0200

    Add an Abstract Form Handler

---
+++ Application/Form/Handler/AbstractFormHandler.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * This file is part of the Black package.
+ *
+ * (c) Alexandre Balmes <alexandre@lablackroom.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Black\Bundle\CommonBundle\Application\Form\Handler;
+
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+abstract class AbstractFormHandler implements HandlerInterface
+{
+    /**
+     * @var \Symfony\Component\Form\FormInterface
+     */
+    protected $form;
+
+    /**
+     * @var null|\Symfony\Component\HttpFoundation\Request
+     */
+    protected $request;
+
+    /**
+     * @param FormInterface    $form
+     * @param RequestStack     $requestStack
+     */
+    public function __construct(
+        FormInterface $form,
+        RequestStack $requestStack
+    ) {
+        $this->form    = $form;
+        $this->request = $requestStack->getCurrentRequest();
+    }
+
+    /**
+     * @return bool|mixed
+     */
+    public function process()
+    {
+        if ('POST' === $this->request->getMethod()) {
+            $this->form->handleRequest($this->request);
+
+            if ($this->form->isValid()) {
+                return $this->form->getData();
+            }
+
+            return $this->stop();
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function stop()
+    {
+        return false;
+    }
+} 
